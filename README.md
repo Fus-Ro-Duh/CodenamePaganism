cfd8f5af07a02d0fa1477222995851202ea446d3
Fus-Ro-Duh <odnoromanenko02@bk.ru>
2023-06-14 19:37:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CodenamePaganism
+
+Developed with Unreal Engine 5
---
+++ Source/CodenamePaganism/CodenamePaganismGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodenamePaganismGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODENAMEPAGANISM_API ACodenamePaganismGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
