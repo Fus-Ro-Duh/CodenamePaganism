feda77d3c62a70b726c694338926a672d2917b13
Fus-Ro-Duh <odnoromanenko02@bk.ru>
2023-07-12 21:41:25 +0300

    12/07 again

--- Source/CodenamePaganism/Private/Weapons/Range/BaseRangeWeapon.cpp
+++ Source/CodenamePaganism/Private/Weapons/Range/BaseRangeWeapon.cpp
@@ -11,6 +11,7 @@
 #include "Weapons/Range/Projectile/BaseProjectile.h"
 #include "Animations/AnimUtils.h"
 #include "Animations/ArrowSpawnAnimNotify.h"
+#include "Animations/LoadAnimNotify.h"
 #include "Engine/EngineTypes.h"
 
 ABaseRangeWeapon::ABaseRangeWeapon()
@@ -103,11 +104,12 @@ void ABaseRangeWeapon::InitAnimations()
 	{
 		ArrowSpawnNotify->OnNotified.AddUObject(this, &ABaseRangeWeapon::SpawnArrow);
 	}
-	else if (true)
+	auto LoadNotify = AnimUtils::FindNotifyByClass<ULoadAnimNotify>(LoadAnimation);
+	if (LoadNotify)
 	{
-
+		LoadNotify->OnNotified.AddUObject(this, &ABaseRangeWeapon::StartIncreasingPower);
 	}
-	else
+	if(!ArrowSpawnNotify || !LoadNotify)
 	{
 		checkNoEntry();
 	}
